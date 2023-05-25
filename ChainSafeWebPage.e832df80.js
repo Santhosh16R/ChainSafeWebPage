3ff4e23fe8a0858f1d20229bd3bc401c969f2d40
Santhosh16R <Santhosh16R@users.noreply.github.com>
2023-05-25 05:11:10 +0000

    deploy: d0351a0801234f60f141f7467d77267b2a5ab6d3

--- ChainSafeWebPage.e832df80.js
+++ ChainSafeWebPage.e832df80.js
@@ -30438,7 +30438,7 @@ let signer;
 document.addEventListener("DOMContentLoaded", loadApp());
 
 async function loadApp() {
-  provider = new _ethers.ethers.providers.Web3Provider(window.okxwallet, "any");
+  provider = new _ethers.ethers.providers.Web3Provider(window.metamask, "any");
   signer = provider.getSigner();
   if (!signer) window.location.reload();
   await provider.send("eth_requestAccounts", []);
@@ -30552,4 +30552,4 @@ function displayResponse(text, response) {
   }
 }
 },{"regenerator-runtime/runtime":"KA2S","ethers":"iS6H","ethers/lib/utils":"if8b"}]},{},["Focm"], null)
-//# sourceMappingURL=/ChainSafeWebPage/ChainSafeWebPage.d288d2b7.js.map
+//# sourceMappingURL=/ChainSafeWebPage/ChainSafeWebPage.e832df80.js.map
